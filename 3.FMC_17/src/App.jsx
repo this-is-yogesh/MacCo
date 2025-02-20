6ec4ba5579278d498cab2d9a52b73054bc3b3915
Yogesh Singh <ayogesh1214@gmail.com>
2025-02-20 23:16:19 +0530

    team player added

--- 3.FMC_17/src/App.jsx
+++ 3.FMC_17/src/App.jsx
@@ -1,29 +1,65 @@
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
   const [player, setPlayer] = useState("");
   const [masterTeam, setMasterTeam] = useState([]);
+  const [playing, setPlaying] = useState([]);
 
   function fnaddplayer(player) {
-    console.log(player);
-    return <button>+</button>;
+    console.log(player, "player");
+    return <button onClick={() => addtoplaying(player)}>+</button>;
   }
   function dladdplayer() {
-    return <button>-</button>;
+    return <button onClick={() => removePlayer(player)}>-</button>;
   }
+
+  function addtoplaying(player) {
+    console.log(player, "adding");
+    setPlaying(prev => {
+      return [...prev, player];
+    });
+  }
+
+  function removePlayer(player) {
+    console.log(player, "removeplaying", playing);
+    setPlaying(prev => {
+      return prev.filter(p => p !== player);
+    });
+  }
+
   function addtoMasterTeam(e) {
-    e.preventDefault()
+    e.preventDefault();
     let tempArr = [...masterTeam];
+    if (!player.length) {
+      return;
+    }
     let obj = {
-      player:player,
+      player: player,
       addplayer: fnaddplayer(player),
       deletePlayer: dladdplayer(player),
     };
     console.log([...tempArr, obj], "masterteam");
     setMasterTeam([...tempArr, obj]);
-    setPlayer("")
+    setPlayer("");
   }
+
+  const PlayingEleven = function () {
+    return (
+      <div>
+        <div>
+          <h1>Playing 11</h1>
+          {playing.map((player, index) => {
+            return (
+              <li key={player + index}>
+                <span>{player}</span>
+              </li>
+            );
+          })}
+        </div>
+      </div>
+    );
+  };
 
   return (
     <div>
@@ -51,6 +87,7 @@ function App() {
           ))}
         </div>
       ) : null}
+      {playing.length ? <PlayingEleven /> : null}
     </div>
   );
 }

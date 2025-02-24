8f7c3d3ed0d47bd75efd83d40d62a2b16e5e3c94
Yogesh Singh <ayogesh1214@gmail.com>
2025-02-24 15:04:37 +0530

    changing boxes color on click

--- 4.FMC_20/src/App.jsx
+++ 4.FMC_20/src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 
 function App() {
   let [sizeProp, setSizeProp] = useState(0);
+  let [boxesObj, setBoxesObj] = useState({});
   function getRows(index) {
     let row = Math.floor(index / sizeProp);
     let col = index % sizeProp;
@@ -11,7 +12,22 @@ function App() {
   useEffect(() => {
     let boxesContainer = document.querySelector(".boxesContainer");
     boxesContainer.style.setProperty("--size", sizeProp);
+    let d = sizeProp * sizeProp;
+    let arr = Array.from({ length: d }, (_, index) => {
+      return { clicked: false, [index]: index };
+    });
+    setBoxesObj(arr);
   }, [sizeProp]);
+  
+  function clickingBox(i) {
+    let arr = boxesObj.map(box => {
+      if (box[i] === i) {
+        box["clicked"] = true;
+      }
+      return box;
+    });
+    setBoxesObj(arr);
+  }
   return (
     <div>
       <input
@@ -21,13 +37,19 @@ function App() {
       />
       <div className="outermostContainer">
         <div className="boxesContainer">
-          {new Array(sizeProp * sizeProp).fill(0).map((box, index) => {
+          {boxesObj.length && boxesObj.map((box, index) => {
             let rc = getRows(index);
             let half = Math.floor(sizeProp / 2);
             if (rc[0] === half && rc[1] > 0) {
               return <div key={index}></div>;
             } else {
-              return <div className="box" key={index}></div>;
+              return (
+                <div
+                  className={`${box.clicked ? "clickedBox" : "box"}`}
+                  key={index}
+                  onClick={() => clickingBox(index)}
+                ></div>
+              );
             }
           })}
         </div>

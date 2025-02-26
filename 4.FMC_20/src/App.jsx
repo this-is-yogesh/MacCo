bd3992900a152847860a86a0d305ba383579fdc6
Yogesh Singh <ayogesh1214@gmail.com>
2025-02-26 20:43:35 +0530

    boxes added

--- 4.FMC_20/src/App.jsx
+++ 4.FMC_20/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 
+let count = 0;
 function App() {
   let [sizeProp, setSizeProp] = useState(0);
   let [boxesObj, setBoxesObj] = useState({});
@@ -14,19 +15,56 @@ function App() {
     boxesContainer.style.setProperty("--size", sizeProp);
     let d = sizeProp * sizeProp;
     let arr = Array.from({ length: d }, (_, index) => {
-      return { clicked: false, [index]: index };
+      let rc = getRows(index);
+      let half = Math.floor(sizeProp / 2);
+      if (rc[0] === half && rc[1] > 0) {
+        return { nonbox: true };
+      } else {
+        return { clicked: false, [index]: index, order: 0 };
+      }
     });
     setBoxesObj(arr);
+    console.log(arr, "arr##");
   }, [sizeProp]);
-  
+
+  function triggerReverse() {
+    let origianlBoxes = boxesObj.filter(b => !b.nonbox);
+    origianlBoxes.sort((a, b) => a["order"] - b["order"]);
+    console.log(origianlBoxes, "origianlBoxesSort");
+    let i = 1;
+    while (i <= origianlBoxes.length) {
+      (function (ind) {
+        setTimeout(() => {
+          let arr = origianlBoxes.map(element => {
+            if (element["order"] === ind) {
+              element["clicked"] = false;
+              return element;
+            } else {
+              return element;
+            }
+          });
+          console.log(arr, "arr^^", ind);
+          setBoxesObj(arr);
+        }, ind * 1000);
+      })(i++);
+    }
+  }
   function clickingBox(i) {
+    count++;
     let arr = boxesObj.map(box => {
       if (box[i] === i) {
         box["clicked"] = true;
+        box["order"] = count;
       }
       return box;
     });
+    console.log(arr, "origianlBoxes");
     setBoxesObj(arr);
+    let origianlBoxes = arr.filter(b => !b.nonbox);
+    console.log(origianlBoxes, "origianlBoxes", count);
+    if (count === origianlBoxes.length) {
+      triggerReverse();
+    }
   }
   return (
     <div>
@@ -37,21 +75,22 @@ function App() {
       />
       <div className="outermostContainer">
         <div className="boxesContainer">
-          {boxesObj.length && boxesObj.map((box, index) => {
-            let rc = getRows(index);
-            let half = Math.floor(sizeProp / 2);
-            if (rc[0] === half && rc[1] > 0) {
-              return <div key={index}></div>;
-            } else {
-              return (
-                <div
-                  className={`${box.clicked ? "clickedBox" : "box"}`}
-                  key={index}
-                  onClick={() => clickingBox(index)}
-                ></div>
-              );
-            }
-          })}
+          {boxesObj.length &&
+            boxesObj.map((box, index) => {
+              let rc = getRows(index);
+              let half = Math.floor(sizeProp / 2);
+              if (rc[0] === half && rc[1] > 0) {
+                return <div key={index}></div>;
+              } else {
+                return (
+                  <div
+                    className={`${box?.clicked ? "clickedBox" : "box"}`}
+                    key={index}
+                    onClick={() => clickingBox(index)}
+                  ></div>
+                );
+              }
+            })}
         </div>
       </div>
     </div>

94fef0e9c0d8a2f7b46247efb894c24bcc86a06f
Yogesh Singh <ayogesh1214@gmail.com>
2025-03-24 14:39:29 +0530

    memoization using map

---
+++ 1.MI_CHAK_1/memoizateAdd/index.js
@@ -0,0 +1,37 @@
+const add = (a, b) => {
+  for (let i = 0; i < 1000000; i++) {}
+  return a + b;
+};
+
+function memoizeOne(callBackFn) {
+  let cacheObject = new Map();
+  return function memoizeAdd(...args) {
+    let key = JSON.stringify(args);
+    if (cacheObject.has(key)) {
+      return cacheObject.get(key);
+    } else {
+      let result = callBackFn(...args);
+      cacheObject.set(key,result)
+      return result;
+    }
+  };
+}
+const memoizeAdd = memoizeOne(add);
+
+console.time("AddMemoize2");
+memoizeAdd(12, 14);
+console.timeEnd("AddMemoize2");
+
+console.time("Add1");
+console.log(memoizeAdd(1, 2));
+console.timeEnd("Add1");
+
+console.time("AddMemoize");
+console.log(memoizeAdd(1, 2));
+console.timeEnd("AddMemoize");
+
+console.time("Add2");
+console.log(memoizeAdd(8, 9));
+console.timeEnd("Add2");
+
+

f71a604bc9f07cc5fbe81a610f657557e8ed4c50
Yogesh Singh <ayogesh1214@gmail.com>
2025-03-24 14:23:45 +0530

    reducing op keys

---
+++ 1.MI_CHAK_1/groupData/index2op.js
@@ -0,0 +1,29 @@
+let Output = {
+  "Sample 1": [
+    {
+      key: "Sample 1",
+      data: "Data1",
+    },
+    {
+      key: "Sample 1",
+      data: "Data1",
+    },
+    {
+      key: "Sample 1",
+      data: "Data1",
+    },
+  ],
+  "Sample 2": [],
+  "Sample 3": [
+    {
+      key: "Sample 3",
+      data: "Data1",
+    },
+  ],
+  "Sample 4": [
+    {
+      key: "Sample 4",
+      data: "Data1",
+    },
+  ],
+};

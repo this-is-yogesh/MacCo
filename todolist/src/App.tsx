5d7697097f8c7289165cce9dc9902fc35885b7d2
Yogesh Singh <ayogesh1214@gmail.com>
2025-04-23 21:41:24 +0530

    visible 2

--- todolist/src/App.tsx
+++ todolist/src/App.tsx
@@ -1,20 +1,42 @@
-import { useState } from "react";
+import { KeyboardEvent, useState, useRef, useEffect } from "react";
 import "./styles/App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [hidePlusAdd, sethidePlusAdd] = useState<boolean>(false);
+  const inputElement = useRef<HTMLInputElement>(null);
+
+  function plusAddTask(): void {
+    sethidePlusAdd(true);
+    inputElement.current?.focus();
+  }
+
 
   return (
     <div>
       <header className="header_app">To Do List Application</header>
       <main className="main_app">
-        <div className="tab_app">
+        <div className="tab_bar">
           <div id="tab_element_1">
             <h3>To Do List</h3>
             <div className="task_body_tab_element1">
-              <div className="plusAddTask_tab_element1">
+              <div
+                className={`plusAddTask_tab_element1 ${
+                  hidePlusAdd ? "hidden" : "visible"
+                } `}
+                onClick={plusAddTask}
+              >
                 <button>+</button>
                 <label>Add Task</label>
+              </div>
+              <div
+                className={`input_tab_element1 ${
+                  hidePlusAdd ? "visible" : "hidden"
+                } `}
+              >
+                <input
+                  ref={inputElement}
+                  onKeyDown={event => showPlusAddTask(event)}
+                />
               </div>
             </div>
           </div>

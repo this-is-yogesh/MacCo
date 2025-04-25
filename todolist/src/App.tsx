82581e505ec21200953cc2ac489fa850b0f6bd3d
Yogesh Singh <ayogesh1214@gmail.com>
2025-04-25 19:38:49 +0530

    app tsx punch

--- todolist/src/App.tsx
+++ todolist/src/App.tsx
@@ -1,27 +1,88 @@
-import { KeyboardEvent, useState, useRef, useEffect } from "react";
+import { KeyboardEvent, useState, useRef, useEffect, ChangeEvent } from "react";
 import "./styles/App.css";
+import useLocalStorage from "./hooks/useLocalStorage";
+import deleteSvg from "../public/assests/delete.svg";
+import editSvg from "../public/assests/edit.svg";
 
+interface Task {
+  taskName: string;
+  taskCompleted: boolean;
+}
 function App() {
   const [hidePlusAdd, sethidePlusAdd] = useState<boolean>(false);
   const inputElement = useRef<HTMLInputElement>(null);
+  const [getValueFromLocalStorage, saveToStorage] = useLocalStorage<Task>();
+  const items: Task[] = getValueFromLocalStorage("key");
+  const [tasks, setTasks] = useState<Task[]>(items);
+  const [currentTaskValue, setCurrentTaskValue] = useState<string>("");
+  const [hoveredIndex, setHoveredIndex] = useState<number>(-1);
 
-  function plusAddTask(): void {
-    sethidePlusAdd(true);
-    inputElement.current?.focus();
-  }
+  useEffect(() => {
+    if (hidePlusAdd && inputElement.current) {
+      inputElement.current.focus();
+    }
+  }, [hidePlusAdd]);
+
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      console.log(currentTaskValue.length, "length");
+      if (
+        inputElement.current &&
+        !inputElement.current.contains(event.target as Node) &&
+        !currentTaskValue.length
+      ) {
+        sethidePlusAdd(false);
+      }
+    }
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [hidePlusAdd, currentTaskValue]);
 
   function showPlusAddTask(event: KeyboardEvent<HTMLInputElement>) {
     if (event.key === "Enter") {
+      const presentStateTask: Task[] = tasks;
+      const newTaskAdded = [
+        ...presentStateTask,
+        {
+          taskName: currentTaskValue,
+          taskCompleted: false,
+        },
+      ];
+      saveToStorage("key", newTaskAdded);
+      setTasks(prev => [
+        ...prev,
+        {
+          taskName: currentTaskValue,
+          taskCompleted: false,
+        },
+      ]);
+      setCurrentTaskValue("");
       sethidePlusAdd(false);
     }
   }
 
-  useEffect(() => {
-    if (hidePlusAdd && inputElement.current) {
+  function addTask() {
+    setCurrentTaskValue("");
+    sethidePlusAdd(true);
+  }
 
-      inputElement.current.focus();
-    }
-  }, [hidePlusAdd]);
+  function onMouseEnterFunction(index: number) {
+    console.log(index, "event*8");
+    setHoveredIndex(index);
+  }
+
+  function onMouseLeaveFunction() {
+    console.log("mouseleave");
+    setHoveredIndex(-1);
+  }
+
+  function addInputToTask(event: ChangeEvent<HTMLInputElement>): void {
+    const value = event.target.value;
+    setCurrentTaskValue(value);
+  }
 
   return (
     <div>
@@ -35,7 +96,7 @@ function App() {
                 className={`plusAddTask_tab_element1 ${
                   hidePlusAdd ? "hidden" : "visible"
                 } `}
-                onClick={plusAddTask}
+                onClick={addTask}
               >
                 <button>+</button>
                 <label>Add Task</label>
@@ -46,10 +107,47 @@ function App() {
                 } `}
               >
                 <input
+                  value={currentTaskValue}
                   ref={inputElement}
+                  onChange={event => addInputToTask(event)}
                   onKeyDown={event => showPlusAddTask(event)}
                 />
               </div>
+            </div>
+            <div className="task_list">
+              {tasks.map((item, index) => (
+                <div
+                  key={index}
+                  className={`task_item ${
+                    hoveredIndex === index ? "hoveredItem" : ""
+                  }`}
+                  onMouseEnter={() => onMouseEnterFunction(index)}
+                  onMouseLeave={onMouseLeaveFunction}
+                >
+                  <input
+                    type="checkbox"
+                    checked={item?.taskCompleted}
+                    onChange={() => {}}
+                  />
+                  <label>{item?.taskName}</label>
+                  {hoveredIndex === index && (
+                    <>
+                      <img
+                        src={deleteSvg}
+                        height={15}
+                        width={15}
+                        alt="deleteIcon"
+                      />
+                      <img
+                        src={editSvg}
+                        height={15}
+                        width={15}
+                        alt="editIcon"
+                      />
+                    </>
+                  )}
+                </div>
+              ))}
             </div>
           </div>
           <div id="tab_element_2">

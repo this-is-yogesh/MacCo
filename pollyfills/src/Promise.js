d9dac4955319e04b68346143aaf2f8f78eae5ad4
Yogesh Singh <ayogesh1214@gmail.com>
2025-05-11 20:06:53 +0530

    custom made

--- pollyfills/src/Promise.js
+++ pollyfills/src/Promise.js
@@ -1,5 +1,38 @@
 class MyPromise {
- 
+  constructor(executor) {
+    this.status = "pending"; // 'fulfilled' or 'rejected'
+    this.value = undefined;
+    this.reason = undefined;
+
+    this.onFulfilledCallbacks = [];
+    this.onRejectedCallbacks = [];
+
+    const resolve = value => {
+      if (this.status === "pending") {
+        this.status = "fulfilled";
+        this.value = value;
+        queueMicrotask(() => {
+          this.onFulfilledCallbacks.forEach(cb => cb(value));
+        });
+      }
+    };
+
+    const reject = reason => {
+      if (this.status === "pending") {
+        this.status = "rejected";
+        this.reason = reason;
+        queueMicrotask(() => {
+          this.onRejectedCallbacks.forEach(cb => cb(reason));
+        });
+      }
+    };
+
+    try {
+      executor(resolve, reject);
+    } catch (err) {
+      reject(err);
+    }
+  }
 
   then(onFulfilled) {
     if (typeof onFulfilled === "function") {

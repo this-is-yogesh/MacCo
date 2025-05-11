cfa24cba578362c5513d4d3994563338e6519c1b
Yogesh Singh <ayogesh1214@gmail.com>
2025-05-11 20:06:38 +0530

    my promise

--- pollyfills/src/Promise.js
+++ pollyfills/src/Promise.js
@@ -1,38 +1,5 @@
 class MyPromise {
-  constructor(executor) {
-    this.status = "pending"; // 'fulfilled' or 'rejected'
-    this.value = undefined;
-    this.reason = undefined;
-
-    this.onFulfilledCallbacks = [];
-    this.onRejectedCallbacks = [];
-
-    const resolve = value => {
-      if (this.status === "pending") {
-        this.status = "fulfilled";
-        this.value = value;
-        queueMicrotask(() => {
-          this.onFulfilledCallbacks.forEach(cb => cb(value));
-        });
-      }
-    };
-
-    const reject = reason => {
-      if (this.status === "pending") {
-        this.status = "rejected";
-        this.reason = reason;
-        queueMicrotask(() => {
-          this.onRejectedCallbacks.forEach(cb => cb(reason));
-        });
-      }
-    };
-
-    try {
-      executor(resolve, reject);
-    } catch (err) {
-      reject(err);
-    }
-  }
+ 
 
   then(onFulfilled) {
     if (typeof onFulfilled === "function") {
@@ -57,4 +24,20 @@ class MyPromise {
   }
 }
 
+const promise1 = new MyPromise((resolve, reject) => {
+  resolve(1);
+  reject(3);
+  // Ignored
+  console.log("hello");
+  setTimeout(() => {
+    console.log("insideTimeout");
+  });
+});
 
+promise1
+  .then(value => {
+    console.log(value, "resolveResult");
+  })
+  .catch(error => {
+    console.log(error, "rejectResult");
+  });

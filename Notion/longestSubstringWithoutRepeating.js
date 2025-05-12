7e4ad03c6090cbae84301b87087e07bd43d8bc42
Yogesh Singh <ayogesh1214@gmail.com>
2025-05-12 22:02:16 +0530

    put  method added

--- Notion/longestSubstringWithoutRepeating.js
+++ Notion/longestSubstringWithoutRepeating.js
@@ -20,3 +20,25 @@ class LRUCache {
     return -1;
   };
 
+  put = function (key, value) {
+    for (let i = 0; i < this.cache.length; i++) {
+      if (this.cache[i][0] === key) {
+        this.cache.splice(i, 1);
+        this.cache.push([key, value]);
+        return;
+      }
+    }
+    if (this.cache.length === this.capacity) {
+      this.cache.shift();
+    }
+    this.cache.push([key, value]);
+    console.log(this.cache, "cache");
+  };
+}
+
+var obj = new LRUCache(2);
+//var param_1 = obj.get(key)
+obj.put(1, 1);
+obj.put(2, 2);
+console.log(obj.get(1));
+obj.put(3, 3);

47fef4d2773783a5535dd3e8eb2beec59449a5ec
Yogesh Singh <ayogesh1214@gmail.com>
2025-06-11 18:09:50 +0530

    main box component

---
+++ MacCod/1.TransferList/transferlist/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import MainBox from "./components/MainBox";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <MainBox />
+    </>
+  );
+}
+
+export default App;

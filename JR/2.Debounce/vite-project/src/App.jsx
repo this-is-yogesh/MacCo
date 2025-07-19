39469f53abe0927cf3ea02dfe5f98195796a9330
Yogesh Singh <ayogesh1214@gmail.com>
2025-07-19 16:08:41 +0530

    debounced0

--- JR/2.Debounce/vite-project/src/App.jsx
+++ JR/2.Debounce/vite-project/src/App.jsx
@@ -1,15 +1,32 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useState, useEffect } from "react";
 import "./App.css";
+import useDebounce from "./customHook/Debounce";
 
 function App() {
   const [value, setValue] = useState("");
   const styles = getStyles();
 
+  const [debounced] = useState(() => {
+    let timeOut = null;
+    console.log("Debounced0:");
+    return function (args, delay) {
+      console.log("Debounced1:", args);
+      if (timeOut) clearTimeout(timeOut);
+      timeOut = setTimeout(() => {
+        callFunction(args);
+      }, delay);
+    };
+  });
+
+  function callFunction(val) {
+    console.log(val, "debounce", val.length);
+  }
+
   function textChange(e) {
     setValue(e.target.value);
+    debounced(e.target.value, 1500);
   }
+
   return (
     <div style={styles.outerBox}>
       <input
@@ -17,7 +34,7 @@ function App() {
         value={value}
         placeholder="write here"
         style={styles.inputBox}
-        onChange={textChange}
+        onChange={e => textChange(e)}
       />
     </div>
   );
@@ -25,15 +42,15 @@ function App() {
 
 function getStyles() {
   return {
-    outerBox: {fontSize:'20px'},
+    outerBox: { fontSize: "20px" },
     inputBox: {
       width: "15em",
       height: "1.5em",
       position: "fixed",
       top: 0,
       left: 0,
-      transform:"translate(30em,10em)",
-      fontSize:'1em'
+      transform: "translate(30em,10em)",
+      fontSize: "1em",
     },
   };
 }

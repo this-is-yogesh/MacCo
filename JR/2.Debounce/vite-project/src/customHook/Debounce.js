3b8e2ddba677ad0e1fe8d68fb5f0d66f7cd40246
Yogesh Singh <ayogesh1214@gmail.com>
2025-07-21 21:31:58 +0530

    use debounce

--- JR/2.Debounce/vite-project/src/customHook/Debounce.js
+++ JR/2.Debounce/vite-project/src/customHook/Debounce.js
@@ -1,16 +1,8 @@
 import { useState } from "react";
 
 function useDebounce(callBack, delay) {
-  const [debounced] = useState(() => {
-    let timeOut = null;
-    return function (...args) {
-      if (timeOut) clearTimeout(timeOut);
-      timeOut = setTimeout(() => {
-        callBack(args);
-      }, delay);
-    };
-  });
-  return debounced
+
+  //return debounced
 }
 
 export default useDebounce;
